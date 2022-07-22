7d77f076aaf460d5b2d01a9e2469701c0729d915
rooru9 <108274846+rooru@users.noreply.github.com>
2022-07-22 20:51:31 +0900

    220722

--- script/index.js
+++ script/index.js
@@ -1,3 +1,15 @@
+function moveSlider(index){
+    $('.control li').css({
+        width:'15px',
+        background: 'url(./images/off.svg) center/cover'
+    }).eq(index).css({
+        width:'35px',
+        background:'url(./images/on.svg) center/cover'
+    });
+};
+
+var cnt=0;
+
 $(function(){
     $('.slide a').eq(0).show();
     $('.slide a:not(:eq(0))').hide();
@@ -9,6 +21,31 @@ $(function(){
     $('.slide a').eq(1).css('backgroundColor','#f5f2e7');
     $('.slide a').eq(2).css('backgroundColor','#ebe4dc');
     $('.slide a').eq(3).css('backgroundColor','#d8e7be');
+
+    // var ww = $(window).width();
+    // if(ww<850){
+    //     $('.event li').eq(0).show();
+    //     $('.event li:not(:eq(0))').hide();
+    //     setInterval(function(){
+    //         $('.event li').eq(0).fadeOut(1000).next().fadeIn(1000).end().appendTo('.event');
+    //     }, 3000);
+    // };
+    
+
+    $('.control li').each(function(i){
+        $(this).attr('data-index',i);
+    }).click(function(){
+        var index = $(this).attr('data-index');
+        moveSlider(index);
+    });
+    setInterval(function(){
+        if(cnt<3){
+            cnt++;
+        }else{
+            cnt=0;
+        }        
+        moveSlider(cnt);    
+    },3000);
 });
 
 // var underline=document.querySelector('.underline');
