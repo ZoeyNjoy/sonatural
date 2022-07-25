5190fbbe679f90241c608504fc860c6eaa00aba9
rooru9 <108274846+rooru@users.noreply.github.com>
2022-07-25 21:29:18 +0900

    220725
    메뉴슬라이드

--- script/index.js
+++ script/index.js
@@ -1,56 +1,32 @@
-function moveSlider(index){
-    $('.control li').css({
-        width:'15px',
-        background: 'url(./images/off.svg) center/cover'
-    }).eq(index).css({
-        width:'35px',
-        background:'url(./images/on.svg) center/cover'
-    });
-};
-
-var cnt=0;
-
 $(function(){
-    $('.slide a').eq(0).show();
-    $('.slide a:not(:eq(0))').hide();
-    setInterval(function(){
-        $('.slide a').eq(0).fadeOut(1000).next().fadeIn(1000).end().appendTo('.slide');
-    }, 3000);
+    $('.slide').bxSlider({
+        mode: 'fade',
+        captions: true,
+        auto: true
+      });
 
     $('.slide a').eq(0).css('backgroundColor','#fad3d2');
     $('.slide a').eq(1).css('backgroundColor','#f5f2e7');
     $('.slide a').eq(2).css('backgroundColor','#ebe4dc');
     $('.slide a').eq(3).css('backgroundColor','#d8e7be');
 
-
-    $('.control li').each(function(i){
-        $(this).attr('data-index',i);
-    }).click(function(){
-        var index = $(this).attr('data-index');
-        moveSlider(index);
+    // menu
+    $('nav .gnb li').on({
+        mouseenter: function() {
+            $(this).parents().find('.sub').show();
+        },
+        mouseleave: function() { 
+            $(this).parents().find('.sub').hide();
+        }
     });
-    setInterval(function(){
-        if(cnt<3){
-            cnt++;
-        }else{
-            cnt=0;
-        }        
-        moveSlider(cnt);    
-    },3000);
-
-
-    // submenu
-    $('nav .sub').hide();
-    
-    $('nav .gnb').mouseover(function(){
-        $(this).next().show();
+    $('nav .sub').on({
+        mouseenter: function() {
+            $(this).show();
+        },
+        mouseleave: function() { 
+            $(this).hide();
+        }
     });
-    $('nav .gnb').mouseout(function(){
-        $(this).next().hide();
-    });
-    
-
-    
 
 
 }); // 끝
