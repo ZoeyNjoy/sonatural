8f1a783e0981f6f80fc721bf859ab3b32a26230d
rooru9 <109156411+ZoeyNjoy@users.noreply.github.com>
2023-01-09 14:23:04 +0900

    230109

--- script/index.js
+++ script/index.js
@@ -3,6 +3,18 @@ $('.cau .box .btn').on('click',function(){
     $('.cau').hide();
 });
 
+var nav = $("header").offset().top;
+$(window).scroll(function() {
+  	var window = $(this).scrollTop();
+
+    // $("main").hide();
+    if(nav <= window){
+      $("header").addClass("fixed");
+    } else {
+      $("header").removeClass("fixed");
+    }
+});
+
 // 메인 슬라이드
 var swiper = new Swiper('.slide', {
     spaceBetween: 0,
@@ -10,9 +22,8 @@ var swiper = new Swiper('.slide', {
     loop: true,
     speed : 1000,
     autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-    allowTouchMove: true,
+      delay: 3000,
+      disableOnInteraction: false,
     },
     navigation: {
       nextEl: '.swiper-button-next',
@@ -25,7 +36,7 @@ var swiper = new Swiper('.slide', {
 
 
 // PC용 메뉴바
-$('nav li').on('mouseenter', function() {
+/* $('nav li').on('mouseenter', function() {
   $(this).parents().find('.sub').show();
 });
 $('nav li').off('mouseleave');
@@ -36,6 +47,10 @@ $('nav .sub').on({
   mouseleave: function() { 
       $(this).hide();
   }
+});*/
+
+$(".nav li .sub").on('mousemove', function () {
+  $(this).next().find('.snb').toggleClass('on');
 });
 
  //모바일 메뉴바
@@ -59,11 +74,41 @@ $(".sub").on('click', function () {
 $(".depth_02 li a").on('click', function () {
     $(this).next().slideToggle();
   });
-  // $('.s_btn').click(function(){
-
-  // });
 
 
+  // 이벤트 슬라이드
+  var swiper = new Swiper(".event_slide", {
+    breakpoints: {
+      1024: {
+        slidesPerView: 'auto',
+        allowTouchMove: false,
+        loop: false,
+        autoplay: false,
+      },
+      768: {
+        slidesPerView: 'auto',
+        allowTouchMove: false,
+        loop: false,
+        autoplay: false,
+      },
+      640: {
+        slidesPerView: 'auto',
+        allowTouchMove: true,
+        loop: true,
+      },
+    },
+    centeredSlides: true,
+    loop: true,
+    speed : 1000,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    spaceBetween: 0,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
 
 
 // 끝
